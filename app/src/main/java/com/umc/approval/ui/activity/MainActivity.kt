97623ad7588b40a86a2311a6810c660efcddf5a1
Ji Seungmin <cswcsm02@gmail.com>
2023-01-08 23:41:46 +0900

    Navigation 기능 추가
    MainActivity와 주요 Fragment들 추가 및 Navigation 기능 적용

---
+++ app/src/main/java/com/umc/approval/ui/activity/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.umc.approval.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.umc.approval.R
+import com.umc.approval.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        navigation()
+    }
+
+    /**
+     * BottonNav에 맞춰 Navigation 해주는 기능
+     * */
+    private fun navigation() {
+        val host = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment? ?: return
+        navController = host.navController
+        binding.navBottom.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/umc/approval/ui/fragment/CommunityFragment.kt
@@ -0,0 +1,26 @@
+package com.umc.approval.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.umc.approval.R
+
+/**
+ * Community View
+ * */
+class CommunityFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_community, container, false)
+    }
+}
