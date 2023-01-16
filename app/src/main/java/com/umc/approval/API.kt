52e449a4a8bfbfbcd6ddc37c88753328b5b801b3
Ji Seungmin <cswcsm02@gmail.com>
2023-01-16 15:29:02 +0900

    [chore] : api key를 local property와 연결

--- app/src/main/java/com/umc/approval/API.kt
+++ app/src/main/java/com/umc/approval/API.kt
@@ -1,7 +1,7 @@
 package com.umc.approval
 
 object API {
-    val GOOGLE_CLIENT_ID = ""
-    val KAKAO_KEY = ""
-    val LOCAL_BASE_URL = ""
+    val GOOGLE_CLIENT_ID = BuildConfig.GOOGLE_CLIENT_ID
+    val KAKAO_KEY = BuildConfig.KAKAO_KEY
+    val LOCAL_BASE_URL = BuildConfig.LOCAL_BASE_URL
 }
