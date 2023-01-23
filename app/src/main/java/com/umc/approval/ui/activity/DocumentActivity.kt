54ba3b36618504b935f57340ee10ad81e406a255
Ji Seungmin <cswcsm02@gmail.com>
2023-01-23 14:55:55 +0900

    Design: Document Activity UI Change
    Document Activity UI Change

--- app/src/main/java/com/umc/approval/ui/activity/DocumentActivity.kt
+++ app/src/main/java/com/umc/approval/ui/activity/DocumentActivity.kt
@@ -3,6 +3,8 @@ package com.umc.approval.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import coil.load
+import com.umc.approval.data.dto.opengraph.OpenGraphDto
 import com.umc.approval.databinding.ActivityDocumentBinding
 import com.umc.approval.ui.adapter.document_comment_activity.DocumentCommentAdapter
 import com.umc.approval.ui.adapter.document_comment_activity.DocumentCommentItem
@@ -16,12 +18,30 @@ class DocumentActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        //back to main activity
+        binding.cancel.setOnClickListener {
+            finish()
+        }
+
+        //서류
         setDocumentData()
 
+        //댓글
+        setComment()
+    }
+
+    private fun setComment() {
         binding.documentCommentRecyclerview.layoutManager = LinearLayoutManager(this)
         val itemList = ArrayList<DocumentCommentItem>()
-        for (i in 1..20){
-            itemList.add(DocumentCommentItem("김부장", "댓글 내용 예시 텍스트 공간입니다. 가나다라마바사아자차카파하 ABCDEFGHIJKLMNOPQRSTUVWXYZ", "12/31 1 시간 전", 50))
+        for (i in 1..20) {
+            itemList.add(
+                DocumentCommentItem(
+                    "김부장",
+                    "댓글 내용 예시 텍스트 공간입니다. 가나다라마바사아자차카파하 ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                    "12/31 1 시간 전",
+                    50
+                )
+            )
         }
         val documentCommentAdapter = DocumentCommentAdapter(itemList)
         documentCommentAdapter.notifyDataSetChanged()
@@ -30,23 +50,24 @@ class DocumentActivity : AppCompatActivity() {
     }
 
     private fun setDocumentData(){
-        binding.name.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n" +
-                " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \n" +
-                "when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n" +
-                "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. \n" +
-                "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, \n"
-
-        binding.title.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n" +
-                " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \n" +
-                "when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n" +
-                "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. \n" +
-                "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, \n"
 
         binding.content.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n" +
                 " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, \n" +
                 "when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n" +
                 "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. \n" +
                 "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, \n"
+
+        var openGraphDto = OpenGraphDto(
+            "https://www.naver.com/",
+            "네이버",
+            "네이버",
+            "네이버",
+            "https://s.pstatic.net/static/www/mobile/edit/2016/0705/mobile_212852414260.png"
+        )
+
+        binding.openGraphImage.load(openGraphDto.image)
+        binding.openGraphUrl.setText(openGraphDto.url)
+        binding.openGraphText.setText(openGraphDto.title)
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
