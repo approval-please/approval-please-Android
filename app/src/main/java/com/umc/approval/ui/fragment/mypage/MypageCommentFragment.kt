cf62580868fa1f8c2432cf5a52a8cb0f948f7fff
yubin <cyoobin@naver.com>
2023-01-31 16:04:59 +0900

    Feat: 마이페이지 각 탭 뷰 연결

--- app/src/main/java/com/umc/approval/ui/fragment/mypage/MypageCommentFragment.kt
+++ app/src/main/java/com/umc/approval/ui/fragment/mypage/MypageCommentFragment.kt
@@ -1,11 +1,25 @@
 package com.umc.approval.ui.fragment.mypage
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.umc.approval.R
+import com.umc.approval.data.dto.approval.get.ApprovalPaper
+import com.umc.approval.data.dto.approval.get.ApprovalPaperDto
 import com.umc.approval.databinding.FragmentMypageCommentBinding
+import com.umc.approval.ui.activity.DocumentActivity
+import com.umc.approval.ui.adapter.approval_fragment.ApprovalPaperListRVAdapter
+import com.umc.approval.ui.fragment.approval.ApprovalBottomSheetDialogStatusFragment
+import com.umc.approval.ui.viewmodel.mypage.MypageViewModel
+
 /*
   MyPage 댓글 tab View
  */
@@ -13,6 +27,8 @@ class MypageCommentFragment : Fragment() {
 
     private var _binding : FragmentMypageCommentBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel by viewModels<MypageViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +40,41 @@ class MypageCommentFragment : Fragment() {
     ): View? {
         _binding = FragmentMypageCommentBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        binding.cgFilter.setOnCheckedStateChangeListener { _, checkedIds ->
+            Log.d("로그", "서류 종류 선택, $checkedIds")
+            // checkedIds에 따라 API 호출, 리사이클러뷰 갱신
+        }
+
+        binding.stateSelect.setOnClickListener {
+            val bottomSheetDialog = ApprovalBottomSheetDialogStatusFragment(binding.stateText.text.toString())
+            bottomSheetDialog.setStyle(
+                DialogFragment.STYLE_NORMAL,
+                R.style.RoundCornerBottomSheetDialogTheme
+            )
+            bottomSheetDialog.show(childFragmentManager, bottomSheetDialog.tag)
+        }
+
+        childFragmentManager
+            .setFragmentResultListener("status", this) { _, bundle ->
+                val result = bundle.getString("result")
+                binding.stateText.text = result
+
+                // 리사이클러뷰 아이템 갱신
+            }
+
+//        val paperRVAdapter = ApprovalPaperListRVAdapter()
+//        binding.rvMypageComment.adapter = paperRVAdapter
+//        binding.rvMypageComment.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+
+//        val talkRVAdapter = CommunityTalkItemRVAdapter()
+//        binding.rvMypageCommunity.adapter = talkRVAdapter
+//        binding.rvMypageCommunity.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+//
+//        val reportRVAdapter = CommunityReportItemRVAdapter()
+//        binding.rvMypageCommunity.adapter = reportRVAdapter
+//        binding.rvMypageCommunity.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+
         return view
     }
 
