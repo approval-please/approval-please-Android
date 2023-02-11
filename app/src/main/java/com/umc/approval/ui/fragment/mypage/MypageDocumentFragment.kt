8ea22196356307152b3f6d68bf56abb33ef3299c
1ny0ung <0828_iyiy@naver.com>
2023-02-12 07:27:12 +0900

    Fix: Mypage 결재서류 탭 상태 선택해도 뷰에서 상태값 반영되지 않는 오류 수정

--- app/src/main/java/com/umc/approval/ui/fragment/mypage/MypageDocumentFragment.kt
+++ app/src/main/java/com/umc/approval/ui/fragment/mypage/MypageDocumentFragment.kt
@@ -69,6 +69,7 @@ class MypageDocumentFragment : Fragment() {
         childFragmentManager
             .setFragmentResultListener("status", this) { _, bundle ->
                 val result = bundle.getString("result")
+                binding.stateText.text = result
                 viewModel.setState(Utils.statusMap[result.toString()]!!)
             }
 
